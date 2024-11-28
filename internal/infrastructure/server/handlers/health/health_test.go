3964851b0c348cd66ace2691dcf2911708f1767f
Gennady Terekhov <ter.ge@yandex.ru>
2024-11-28 12:35:35 +0300

    issue-12 fixed after test

---
+++ internal/infrastructure/server/handlers/health/health_test.go
@@ -0,0 +1,30 @@
+package health
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gennadyterekhov/auth-microservice/internal/tests/inits"
+	"github.com/gennadyterekhov/auth-microservice/internal/tests/suites"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type healthSuite struct {
+	suites.WithServer
+}
+
+func TestHealthHandler(t *testing.T) {
+	suite.Run(t, new(healthSuite))
+}
+
+func (suite *healthSuite) SetupSuite() {
+	inits.InitFactorySuite(suite)
+
+	suite.SetServer(httptest.NewServer(Handler(NewController())))
+}
+
+func (suite *healthSuite) TestHealth() {
+	statusCode, _ := suite.SendGet("/health", "")
+	require.Equal(suite.T(), 200, statusCode)
+}
