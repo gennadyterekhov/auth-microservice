d9995f1cf5f2c62b3cedfbb17cc489440f280721
Gennady Terekhov <ter.ge@yandex.ru>
2024-11-28 15:59:04 +0300

    issue-12 fixed after test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,21 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/gennadyterekhov/auth-microservice/internal/app"
-	"github.com/gennadyterekhov/auth-microservice/internal/logger"
+	"github.com/gennadyterekhov/auth-microservice/internal/project/config"
 )
 
 func main() {
 	fmt.Println("server initialization")
-
-	appInstance := app.New()
+	serverConfig, appInstance, err := getAppInstance()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	fmt.Println("got app instance")
 
 	fmt.Println("server initialized successfully")
 
-	err := appInstance.StartServer()
+	fmt.Println("listening with https on " + serverConfig.Addr)
+
+	err = http.ListenAndServeTLS(serverConfig.Addr, "cmd/server/server.crt", "cmd/server/server.key", appInstance.Router())
 	if err != nil {
-		logger.Errorln(err.Error())
-		panic(err)
+		log.Fatalln(err.Error())
 	}
+}
+
+func getAppInstance() (*config.Config, *app.App, error) {
+	serverConfig, err := config.New()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	fmt.Println("got server config")
+
+	appInstance, err := app.New(serverConfig.DBDsn)
+	return serverConfig, appInstance, err
 }
